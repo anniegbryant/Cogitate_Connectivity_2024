e0711b7b834ad2de4a8bb4b5c01c39333e85537c
anniegbryant <anniegbryant@gmail.com>
2024-06-14 17:21:36 +0900

    Final figures

--- classification/fit_all_classifiers.py
+++ classification/fit_all_classifiers.py
@@ -141,78 +141,75 @@ if not os.path.isfile(f"{classification_res_path}/comparing_between_relevance_ty
     for meta_roi_comparison in meta_roi_comparisons:
         print("ROI Comparison:" + str(meta_roi_comparison))
         ROI_from, ROI_to = meta_roi_comparison
-        for stimulus_type in stimulus_types:
-            print("Stimulus type:" + str(stimulus_type))
-            for stimulus_presentation in stimulus_presentation_comparisons:
-                print("Stimulus presentation:" + str(stimulus_presentation))
-                # Finally, we get to the final dataset
-                final_dataset_for_classification = all_pyspi_res.query("meta_ROI_from == @ROI_from & meta_ROI_to == @ROI_to & stimulus_type == @stimulus_type & stimulus == @stimulus_presentation").reset_index(drop=True).drop(columns=['index'])
+        for stimulus_presentation in stimulus_presentation_comparisons:
+            print("Stimulus presentation:" + str(stimulus_presentation))
+            # Finally, we get to the final dataset
+            final_dataset_for_classification = all_pyspi_res.query("meta_ROI_from == @ROI_from & relevance_type in @relevance_type_comparisons and meta_ROI_to == @ROI_to & stimulus == @stimulus_presentation").reset_index(drop=True).drop(columns=['index'])
 
-                for SPI in final_dataset_for_classification.SPI.unique():
+            for SPI in final_dataset_for_classification.SPI.unique():
 
-                    # Extract this SPI
-                    this_SPI_data = final_dataset_for_classification.query(f"SPI == '{SPI}'")
+                # Extract this SPI
+                this_SPI_data = final_dataset_for_classification.query(f"SPI == '{SPI}'")
 
-                    # Find overall number of rows
-                    num_rows = this_SPI_data.shape[0]
+                # Find overall number of rows
+                num_rows = this_SPI_data.shape[0]
 
-                    # Extract SPI values
-                    this_column_data = this_SPI_data["value"]
+                # Extract SPI values
+                this_column_data = this_SPI_data["value"]
 
-                    # Find number of NaN in this column 
-                    num_NaN = this_column_data.isna().sum()
-                    prop_NaN = num_NaN / num_rows
+                # Find number of NaN in this column 
+                num_NaN = this_column_data.isna().sum()
+                prop_NaN = num_NaN / num_rows
 
-                    # Find mode and SD
-                    column_mode_max = this_column_data.value_counts().max()
-                    column_SD = this_column_data.std()
+                # Find mode and SD
+                column_mode_max = this_column_data.value_counts().max()
+                column_SD = this_column_data.std()
 
-                    # If 0% < num_NaN < 10%, impute by the mean of each component
-                    if 0 < prop_NaN < 0.1:
-                        values_imputed = (this_column_data
-                                            .transform(lambda x: x.fillna(x.mean())))
+                # If 0% < num_NaN < 10%, impute by the mean of each component
+                if 0 < prop_NaN < 0.1:
+                    values_imputed = (this_column_data
+                                        .transform(lambda x: x.fillna(x.mean())))
 
-                        this_column_data = values_imputed
-                        print(f"Imputing column values for {SPI}")
-                        this_SPI_data["value"] = this_column_data
+                    this_column_data = values_imputed
+                    print(f"Imputing column values for {SPI}")
+                    this_SPI_data["value"] = this_column_data
 
-                    # If there are: 
-                    # - more than 10% NaN values;
-                    # - more than 90% of the values are the same; OR
-                    # - the standard deviation is less than 1*10**(-10)
-                    # then remove the column
-                    if prop_NaN > 0.1 or column_mode_max / num_rows > 0.9 or column_SD < 1*10**(-10):
-                        print(f"{SPI} has low SD: {column_SD}, and/or too many mode occurences: {column_mode_max} out of {num_rows}, and/or {100*prop_NaN}% NaN")
-                        continue
+                # If there are: 
+                # - more than 10% NaN values;
+                # - more than 90% of the values are the same; OR
+                # - the standard deviation is less than 1*10**(-10)
+                # then remove the column
+                if prop_NaN > 0.1 or column_mode_max / num_rows > 0.9 or column_SD < 1*10**(-10):
+                    print(f"{SPI} has low SD: {column_SD}, and/or too many mode occurences: {column_mode_max} out of {num_rows}, and/or {100*prop_NaN}% NaN")
+                    continue
 
-                    # Start an empty list for the classification results
-                    SPI_combo_res_list = []
+                # Start an empty list for the classification results
+                SPI_combo_res_list = []
 
-                    # Define classification model
-                    model = LogisticRegression(penalty='l1', C=1, solver='liblinear', random_state=127)
-                    pipe = Pipeline([('scaler', MixedSigmoidScaler(unit_variance=True)), 
-                                            ('model', model)])
+                # Define classification model
+                model = LogisticRegression(penalty='l1', C=1, solver='liblinear', random_state=127)
+                pipe = Pipeline([('scaler', MixedSigmoidScaler(unit_variance=True)), 
+                                        ('model', model)])
 
-                    # Fit classifier
-                    X = this_SPI_data.value.to_numpy().reshape(-1, 1)
-                    y = this_SPI_data.relevance_type.to_numpy().reshape(-1, 1)
-                    groups = this_SPI_data.subject_ID.to_numpy().reshape(-1, 1)
+                # Fit classifier
+                X = this_SPI_data.value.to_numpy().reshape(-1, 1)
+                y = this_SPI_data.relevance_type.to_numpy().reshape(-1, 1)
+                groups = this_SPI_data.subject_ID.to_numpy().reshape(-1, 1)
 
-                    group_stratified_CV = StratifiedGroupKFold(n_splits = 10, shuffle = True, random_state=127)
+                group_stratified_CV = StratifiedGroupKFold(n_splits = 10, shuffle = True, random_state=127)
 
-                    this_classifier_res = cross_validate(pipe, X, y, groups=groups, cv=group_stratified_CV, scoring="accuracy", n_jobs=1, 
-                                                                return_estimator=False, return_train_score=False)["test_score"].mean()
-                    
-                    this_SPI_relevance_results_df = pd.DataFrame({"SPI": [SPI], 
-                                                        "meta_ROI_from": [ROI_from],
-                                                        "meta_ROI_to": [ROI_to],
-                                                        "stimulus_type": [stimulus_type],
-                                                        "stimulus_presentation": [stimulus_presentation],
-                                                        "comparison": ["Relevant non-target vs. Irrelevant"], 
-                                                        "accuracy": [this_classifier_res]})
-                    
-                    # Append to growing results list
-                    comparing_between_relevance_types_classification_results_list.append(this_SPI_relevance_results_df)
+                this_classifier_res = cross_validate(pipe, X, y, groups=groups, cv=group_stratified_CV, scoring="accuracy", n_jobs=1, 
+                                                            return_estimator=False, return_train_score=False)["test_score"].mean()
+                
+                this_SPI_relevance_results_df = pd.DataFrame({"SPI": [SPI], 
+                                                    "meta_ROI_from": [ROI_from],
+                                                    "meta_ROI_to": [ROI_to],
+                                                    "stimulus_presentation": [stimulus_presentation],
+                                                    "comparison": ["Relevant non-target vs. Irrelevant"], 
+                                                    "accuracy": [this_classifier_res]})
+                
+                # Append to growing results list
+                comparing_between_relevance_types_classification_results_list.append(this_SPI_relevance_results_df)
 
     comparing_between_relevance_types_classification_results = pd.concat(comparing_between_relevance_types_classification_results_list).reset_index(drop=True)
     comparing_between_relevance_types_classification_results.to_csv(f"{classification_res_path}/comparing_between_relevance_types_classification_results.csv", index=False)
